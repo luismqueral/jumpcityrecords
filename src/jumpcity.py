bc4968e3773eb951cc0b0d15fe9a61831c498049
Michiel Overtoom <motoom@xs4all.nl>
2014-10-26 14:15:01 +0100

    Using soxi to determine the duration of soundfiles.

--- src/jumpcity.py
+++ src/jumpcity.py
@@ -7,6 +7,7 @@ Generic routines for the jumpcity project. Software by Michiel Overtoom, motoom@
 
 import string
 import random
+import subprocess
 
 def randomname():
     allowed = string.letters + string.digits
@@ -18,6 +19,26 @@ def rnd(v):
     return random.uniform(0, v) 
 
 
+def hhmmss2seconds(x):
+    "Convert a string like '01:23:58.230' to seconds."
+    h, m, s = x.split(":")
+    return float(h) * 3600 + float(m) * 60 + float(s)
+
+    
+def soundfileduration(fn):
+    "Use soxi to determine the duration of a sound file, in seconds."
+    p = subprocess.Popen(("soxi", fn), stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    for line in output.splitlines():
+        if line.startswith("Duration"):
+            for part in line.split(" "):
+                if "." in part:
+                    return hhmmss2seconds(part)
+    return None
+
+
 if __name__ == "__main__":
     print randomname()
     print rnd(100)
+    fn = "/Users/user/Desktop/jumpcityrecords-audio/backgrounds/nature/Madacy - Earth sounds - Ocean surf.mp3"
+    print soundfileduration(fn)
