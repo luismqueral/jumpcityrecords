83b91729419d010e0e645badc77ef6e311d0ea4c
Michiel Overtoom <motoom@xs4all.nl>
2014-11-02 13:56:21 +0100

    README generation

--- src/jumpcity.py
+++ src/jumpcity.py
@@ -16,18 +16,33 @@ def generatealbum():
     """ Generate a complete album. """
     at = albumtracks.AlbumTracks()
     while True:
-        name, tracks = at.generatealbum()
-        albumdir = os.path.join("..", "_albums", name)
+        albumname, tracks = at.generatealbum()
+        albumdir = os.path.join("..", "_albums", albumname)
         if os.path.exists(albumdir): # Don't overwrite existing albums with the same name.
             continue
         break
-    print "Making new album '%s'" % name
+    print "\nMaking new album '%s'" % albumname
     os.mkdir(albumdir)
-    for track in tracks:
+    maxtracknamelen = max(len(track[0]) for track in tracks)
+    
+    readme = []
+    for nr, track in enumerate(tracks):
         trackname, duration = track
+        # TODO: catch exception and re-generate track in case of error
         trackfilename = composetrack.generate(duration, mp3=True)
-        os.rename(trackfilename, os.path.join(albumdir, trackname + ".mp3"))
-
+        # TODO: Problematic characters in filenames, eg. ?, /, \, :
+        destname = "%02d %s - %s" % (nr + 1, albumname, trackfilename)
+        os.rename(trackfilename, os.path.join(albumdir, destname))
+        # An entry in the readme.
+        minutes = int(duration / 60)
+        seconds = duration - minutes * 60
+        padspace = " " if minutes < 10 else ""
+        line = u"%02d %-*s %s%d:%02d" % (nr + 1, maxtracknamelen, trackname, padspace, minutes, seconds)
+        readme.append(line)
+    readme = "\n".join(readme)
+    with open(os.path.join(albumdir, "readme.md")) as of:
+        of.write(readme)
+        
 
 if __name__ == "__main__":
     for i in xrange(5):
