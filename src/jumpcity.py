6b70daae7c6a3236dc47ab8c24005679b050457c
Michiel Overtoom <motoom@xs4all.nl>
2014-11-02 14:06:57 +0100

    Some corrections

--- src/jumpcity.py
+++ src/jumpcity.py
@@ -10,7 +10,7 @@ import albumtracks
 import composetrack
 import utils
 import os
-
+import codecs
 
 def generatealbum():
     """ Generate a complete album. """
@@ -28,10 +28,16 @@ def generatealbum():
     readme = []
     for nr, track in enumerate(tracks):
         trackname, duration = track
-        # TODO: catch exception and re-generate track in case of error
-        trackfilename = composetrack.generate(duration, mp3=True)
-        # TODO: Problematic characters in filenames, eg. ?, /, \, :
-        destname = "%02d %s - %s" % (nr + 1, albumname, trackfilename)
+        # Catch exception and re-generate track in case of error.
+        while True:
+            try:
+                trackfilename = composetrack.generate(duration, mp3=True)
+            except ValueError:
+                continue
+            break
+        # TODO: What about problematic characters in filenames, eg. ?, /, \, :
+        # TODO: Output format configurable (MP3, FLAC, etc)
+        destname = u"%02d %s - %s.mp3" % (nr + 1, albumname, trackname)
         os.rename(trackfilename, os.path.join(albumdir, destname))
         # An entry in the readme.
         minutes = int(duration / 60)
@@ -39,10 +45,10 @@ def generatealbum():
         padspace = " " if minutes < 10 else ""
         line = u"%02d %-*s %s%d:%02d" % (nr + 1, maxtracknamelen, trackname, padspace, minutes, seconds)
         readme.append(line)
-    readme = "\n".join(readme)
-    with open(os.path.join(albumdir, "readme.md")) as of:
+    readme = u"\n".join(readme)
+    with codecs.open(os.path.join(albumdir, "readme.md"), "w", "utf8") as of:
         of.write(readme)
-        
+
 
 if __name__ == "__main__":
     for i in xrange(5):
