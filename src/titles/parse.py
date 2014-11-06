ad719e82433df07b3ce81e4699ee82076e155187
Michiel Overtoom <motoom@xs4all.nl>
2014-11-06 20:49:54 +0100

    Disallow titles with accented characters (filesystem encoding incompatibility)

--- src/titles/parse.py
+++ src/titles/parse.py
@@ -48,6 +48,14 @@ for fn in glob.glob("pagecounts-????????-??????"):
         if "?" in rest or "*" in rest:
             continue # Wildcards in filenames is also a bad idea.
         title, _, _ = rest.rsplit(" ", 2)
+        # At the moment, the combination of nginx and the filesystem have some encoding issues, so no accented chars in titles...
+        containsnonascii = False
+        for c in title:
+            if ord(c) > 127:
+                containsnonascii = True
+                break
+        if containsnonascii:
+            continue        
         titles[project].add(title)
         successes += 1
 
